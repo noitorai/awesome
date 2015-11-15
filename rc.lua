6da77f09a389b5be5d2c4163b5301c755e1cb300
ISHII Masaru <noitorai555+contact@gmail.com>
2015-11-15 19:24:47 +0900

    fix nm-applet duplication

--- rc.lua
+++ rc.lua
@@ -499,5 +499,5 @@ client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_n
 -- }}}
 
 awful.util.spawn_with_shell("xmodmap /home/ishii/.xmodmap")
-awful.util.spawn_with_shell("nm-applet")
+awful.util.spawn_with_shell("pgrep nm-applet >/dev/null || nm-applet")
 awful.util.spawn_with_shell("xscreensaver -no-splash")
